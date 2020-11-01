adbe7d67b814704e3fddaca92eb5330a1b2e62d0
buntybru@gmail.com <buntybru@gmail.com>
2020-11-01 20:17:27 +0530

    changes

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 class comments {
-  constructor(id, name, handle, content, upvotes, downvotes, parentId) {
+  constructor(id, name, handle, content, parentId) {
     this.id = id;
     this.name = name;
     this.handle = handle;
     this.content = content;
-
     this.childrenIds = [];
     this.parentId = parentId;
   }
@@ -17,36 +16,77 @@ const replyBtns = document.querySelectorAll("#reply");
 let rootCommentList = [];
 
 addButton.addEventListener("click", (event) => {
-  let name = document.getElementById("name").value;
-  let handle = document.getElementById("handle").value;
-  let content = document.getElementById("comment").value;
+  console.log("parent-name", event.target.className);
+  if (event.target.className == "add-btn") {
+    let name = document.getElementById("name").value;
+    let handle = document.getElementById("handle").value;
+    let content = document.getElementById("comment").value;
 
-  if (name.trim() !== "" || handle.trim() !== "" || content.trim() !== "") {
-    addComment(name, handle, content);
-    document.getElementById("name").value = "";
-    document.getElementById("handle").value = "";
-    document.getElementById("comment").value = "";
+    if (name.trim() !== "" || handle.trim() !== "" || content.trim() !== "") {
+      addComment(name, handle, content, null, parentUl, null);
+      document.getElementById("name").value = "";
+      document.getElementById("handle").value = "";
+      document.getElementById("comment").value = "";
+    }
   }
 });
 
-let addComment = (name, handle, content) => {
+let addComment = (name, handle, content, parentId, parentComp, refNode) => {
   let id = ID();
-  let comment = new comments(id, name, handle, content, null);
-
+  let comment = new comments(id, name, handle, content, parentId);
+  rootCommentList.push(comment);
   let elem = document.createElement("li");
   elem.classList = "elemclass";
   elem.innerHTML = `<p class='commentContent'> ${content} </p>  <p class='commentName'> - ${name}</p>  <button id='reply' class='reply-${id}'>Reply</button> `;
-  parentUl.append(elem);
+  if (parentId == null) {
+    parentComp.append(elem);
+  } else {
+    parentComp.insertBefore(elem, refNode);
+  }
 };
 
 parentUl.addEventListener("click", (event) => {
-  if (event.target.className.split("-")[0] == "reply") {
+  let levelid = event.target.className.split("-")[0];
+  let identity = event.target.className.split("-")[1];
+  if (levelid == "reply") {
     let parentElem = parentUl.querySelector("." + event.target.className)
       .parentElement;
     let elem = document.createElement("ul");
-    elem.innerHTML = `<li>Hello</li> <li>Hello</li> <li>Hello</li> `;
+    elem.innerHTML = `
+	<li class="replyform-${identity}">
+	<div class="comment-input-row">
+	<div>
+		<input type="text" placeholder="Name" id="name-${identity}" class="name-handle" />
+	</div>
+	</div>
+	<div>
+	<textarea rows="5" id="content-${identity}" class="comment-box" placeholder="Your reply...."></textarea>
+	<div>
+		<button class="addreply-${identity}" >Submit</button>
+	</div>
+	</div>
+	</li>`;
 
-    parentElem.append(elem);
+    let childListElem = parentElem.querySelector(".replyform-" + identity);
+
+    if (childListElem == null) {
+      parentElem.append(elem);
+    }
+  } else if (levelid == "addreply") {
+    let parentElem = parentUl.querySelector("." + event.target.className)
+      .parentElement.parentElement.parentElement.parentElement;
+    console.log("add reply", parentElem);
+
+    let name = document.getElementById("name-" + identity).value;
+    let content = document.getElementById("content-" + identity).value;
+
+    let referenceNode = parentElem.querySelector(".replyform-" + identity);
+
+    if (name.trim() !== "" || content.trim() !== "") {
+      addComment(name, handle, content, identity, parentElem, referenceNode);
+      document.getElementById("name-" + identity).value = "";
+      document.getElementById("content-" + identity).value = "";
+    }
   }
 });
 
